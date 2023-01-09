3f8d98d1cc710710d39dc97d1c42ce2fbcb12be2
eJwtzE2PwiAUheH/wroLaCVt2fqRzGRiYsbJLAnQq0OLXIRWR43/XWK6OznJ8z6IMgZS2uMAngjSVhraqqYtVbXm2tS0YcArxjkwMIsDb0ooDWWkICHixXYQZ/qYS3KcU8c/lBu/6rE/19vzT7P6aod135f7z+Vmd/+4XwI1v9/M59SbyPEWIDsNKkLMbzL4PlR3sl6ESTtr5AC3IoLqBMZjHgGLKUEUcFLWFVeMw8HhNWP4DzZCkmokwk/OPZ8vJpVP6w== <esenia1317@gmail.com>
2023-01-09 12:17:57 +0700

    added task

---
+++ задание/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите длину массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[size];
+int i = 0;
+int secondSize = 0;
+
+Console.WriteLine("Введите элементы массива: ");
+while(size != i)
+{
+    string elements = Console.ReadLine();
+    if(elements.Length <= 3) secondSize++;
+    array[i] = elements;
+    i++;
+}
+
+string[] secondArray = new string[secondSize];
+int count = 0;
+while(size != count)
+{
+    if(array[count].Length <= 3)
+    {
+       secondArray[secondSize-1] = array[count];
+       secondSize--;
+    }
+    count ++;
+}
+
+Console.Write($"[{string.Join(", ", array)}]");
+Console.Write(" =====> ");
+Console.Write($"[{string.Join(", ", secondArray)}]");
